8d66a270740ade21659484d485616dc7b155911d
Jae Y Veenstra <116043029+Jaewalk08@users.noreply.github.com>
2022-12-02 22:03:03 -0600

    creat java file

---
+++ java/main.js
@@ -0,0 +1,14 @@
+const cursorTag = document.querySelector('.circle-explore');
+const circle = cursorTag.querySelector('.icon-explore')
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)
